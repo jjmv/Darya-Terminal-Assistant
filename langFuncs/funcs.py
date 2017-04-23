13814975d80f76984a9d19bfca0f37882748ef7d
Garyi <hi.ed@hotmail.com>
2017-04-22 20:18:29 -0500

    info.json added to my plugins

--- langFuncs/funcs.py
+++ langFuncs/funcs.py
@@ -1,12 +1,8 @@
 from Plugins.DocPlugin import funcs as docfuncs
 from Plugins.CodePlugin import funcs as codefuncs
 options = [
-    "code", # Code generation options
     "-c", # Code generation options
-    "-!",
     "-w", # Code question options
-    "what",  # Code question options
-    "-?", # Code question options
 ]
 
 def getOption(ucommand, commandFromUser):
@@ -17,7 +13,7 @@ def getOption(ucommand, commandFromUser):
         return rawCommand[1] + " not finded"
 
 def executeOption(rawCommand):
-    if (rawCommand[1] == "-w" or rawCommand[1] == "what" or rawCommand[1] == "-?"):
+    if (rawCommand[1] == "-w"):
         return docfuncs.getDoc(rawCommand[0], rawCommand[2])
-    if (rawCommand[1] == "-c" or rawCommand[1] == "code" or rawCommand[1] == "-!" ):
+    if (rawCommand[1] == "-c"):
         return codefuncs.switchoflangs(rawCommand)
