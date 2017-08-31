87b0d844743916c77645a765a90f661d4ac4d1d7
Garyi <hi.ed@hotmail.com>
2017-08-31 15:48:09 +0000

    hotfix installer 1

--- darya_install.sh
+++ darya_install.sh
@@ -1,5 +1,6 @@
+cd ..
 mv Darya-Terminal-Assistant dlib
 sudo mv dlib/ /usr/local/bin/dlib
 sudo mv /usr/local/bin/dlib/darya /usr/local/bin/darya
 sudo chmod 755 /usr/local/bin/darya
-sudo chmod 755 dlib/
+sudo chmod 755 /usr/local/bin/dlib/
