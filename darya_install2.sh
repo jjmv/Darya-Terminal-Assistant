8392dd84462558fac2ebebb9e05d0a93bc3f848e
Garyi <hi.ed@hotmail.com>
2017-08-31 15:52:09 +0000

    deleted the second installer

--- darya_install2.sh
+++
@@ -1,4 +0,0 @@
-git clone https://github.com/Garyi/Darya-Terminal-Assistant.git dlib
-sudo mv dlib/ /usr/local/bin/dlib
-sudo mv /usr/local/bin/dlib/darya /usr/local/bin/darya
-sudo chmod 755 /usr/local/bin/darya
