74609eea11cf9964cf6bb5e43f0851f6b8505e94
Garyi <hi.ed@hotmail.com>
2017-08-31 15:38:25 +0000

    new instalation script

--- darya_install.sh
+++ darya_install.sh
@@ -1,5 +1,5 @@
-cd ..
 mv Darya-Terminal-Assistant dlib
 sudo mv dlib/ /usr/local/bin/dlib
 sudo mv /usr/local/bin/dlib/darya /usr/local/bin/darya
 sudo chmod 755 /usr/local/bin/darya
+sudo chmod 755 dlib/
